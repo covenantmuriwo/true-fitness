db2f41f60bde29fe2560c4e851ed6788b469fe2a
Covenant <muriwocovenant07@gmail.com>
2026-04-01 13:03:53 +0530

    Fix CORS for Netlify domain1

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
